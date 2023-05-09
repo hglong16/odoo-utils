afe9e618612dccef1b60d78047cd50dbd52906c6
hglong16 <intihad.vuong@gmail.com>
2023-05-09 11:14:24 +0700

    update manifest

---
+++ __manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "odoo-utils",
+    "cateogry": "Hidden",
+    "version": "1.0",
+    "description": "zix",
+    "depends": ["base"],
+    "data": [],
+    "assets": [],
+    "bootstrap": True,
+    "license": "LGPL-3",
+}
